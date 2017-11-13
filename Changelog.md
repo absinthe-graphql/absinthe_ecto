60f6c328f32296522e584c145699c6174cb171c0
Ben Wilson <benwilson512@gmail.com>
2017-11-13 18:09:47 -0500

    update for absinthe 1.4.0

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,8 @@
+# 0.1.3
+
+- Support Absinthe 1.4.0
+- Fix compilation warnings
+
 # 0.1.1
 
 - Add assoc/3 to permit query function
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Absinthe.Ecto.Mixfile do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.1.3"
 
   def project do
     [app: :absinthe_ecto,
@@ -42,7 +42,7 @@ defmodule Absinthe.Ecto.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:absinthe, "~> 1.3.0"},
+      {:absinthe, "~> 1.3.0 or ~> 1.4.0"},
       {:ecto, ">= 0.0.0"},
       {:ex_doc, ">= 0.0.0", only: [:dev]},
       {:postgrex, ">= 0.13.0", only: [:test]},
